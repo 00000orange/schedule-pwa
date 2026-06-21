7e0665967eccff57a079583cc7d2bc73df8fd9a4
00000orange <84y4nyngpb@privaterelay.appleid.com>
2026-06-22 03:21:04 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', () => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open('schedule-v2').then((cache) => {
-      return cache.addAll(['/index.html']);
-    })
-  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', () => {
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => resp || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
