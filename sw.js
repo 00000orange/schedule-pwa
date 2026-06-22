15ed5dd29c42268810e4252534423b7bdf72fe0f
00000orange <84y4nyngpb@privaterelay.appleid.com>
2026-06-22 10:37:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('fetch', (event) => {
+self.addEventListener('install', () => {
+  self.skipWaiting(); // 立即激活新版本，不等待旧版本关闭
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim()); // 立即控制所有页面
+});
+
+self.addEventListener('fetch', event => {
+  // 总是从网络获取最新资源，不使用缓存，保证数据同步
   event.respondWith(fetch(event.request));
 });
